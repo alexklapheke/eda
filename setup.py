6f861ddb60b66600f2b4de171c73b339bab4c4be
Alex Klapheke <alexklapheke@gmail.com>
2021-01-05 14:00:47 -0500

    Fix package info

--- setup.py
+++ setup.py
@@ -1,8 +1,8 @@
 from setuptools import setup
 
 setup(
-    name="EDA",
-    version="0.1",
+    name="eda",
+    version="0.1.2020-01-04",
     description=" Basic EDA tools for data science",
     url="https://github.com/alexklapheke/eda",
     author="Alex Klapheke",
